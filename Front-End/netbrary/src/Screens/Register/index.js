5bcfd4db165e2caa97d5e9fa415046ddb632100e
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-12 09:55:13 -0300

    Adicionado tela registro

---
+++ Front-End/netbrary/src/Screens/Register/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+
+
+const Register = () => {
+  return (
+    <Grid
+  container
+  spacing={0}
+  direction="column"
+  alignItems="center"
+  justifyContent="center"
+  style={{ minHeight: '100vh' }}
+>
+
+  <Grid item xs={3}>
+   Oioi
+  </Grid>   
+   
+</Grid> 
+  )
+}
+export default Register
+
