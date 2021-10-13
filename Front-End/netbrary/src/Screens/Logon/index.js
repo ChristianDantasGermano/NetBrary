1e0b1279a2c0b2bdef91e67fb365f31d2aae511e
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-13 20:30:24 -0300

    Adicionado componentes faltantes da tela login

--- Front-End/netbrary/src/Screens/Logon/index.js
+++ Front-End/netbrary/src/Screens/Logon/index.js
@@ -1,25 +1,15 @@
-import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 import {
   Grid,
   Box,
   Card,
-  CardActions,
   CardContent,
   Button,
   Typography,
   TextField,
 } from "@material-ui/core";
-
-const bull = (
-  <Box
-    component="span"
-    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-  >
-    •
-  </Box>
-);
 
 const Logon = () => {
   return (
@@ -35,47 +25,40 @@ const Logon = () => {
         <Card sx={{ minWidth: 500 }}>
           <CardContent>
             <Typography sx={{ textAlign: "center" }} variant="h2">
-              NetBrary
+              Login
             </Typography>
-            <Grid
-              container
-              direction="column"
-              justifyContent="center"
-              spacing="center"
-              alignItems="stretch"
-            >
-              <Grid item>
-                <TextField
-                  id="standard-basic"
-                  label="Standard"
-                  variant="standard"
-                  fullWidth
-                />
+            <Box mx={4}>
+              <Grid
+                container
+                direction="column"
+                justifyContent="center"
+                spacing="center"
+                alignItems="stretch"
+              >
+                <Grid item>
+                  <TextField
+                    id="standard-basic"
+                    label="Email"
+                    variant="standard"
+                    fullWidth
+                  />
+                </Grid>
+                <Grid item>
+                  <TextField
+                    id="standard-basic"
+                    label="Senha"
+                    variant="standard"
+                    fullWidth
+                  />
+                </Grid>
               </Grid>
-              <Grid item>
-                <TextField
-                  id="standard-basic"
-                  label="Standard"
-                  variant="standard"
-                  fullWidth
-                />
-              </Grid>
-            </Grid>
-            <Typography variant="h5" component="div">
-              be{bull}nev{bull}o{bull}lent
-            </Typography>
-            <Typography sx={{ mb: 1.5 }} color="text.secondary">
-              adjective
-            </Typography>
-            <Typography variant="body2">
-              well meaning and kindly.
-              <br />
-              {'"a benevolent smile"'}
-            </Typography>
+            </Box>
+            <Box mx={6} my={4}>
+              <Button component={Link} to={"/register"}variant="contained" fullWidth>
+                Logar
+              </Button>
+            </Box>
           </CardContent>
-          <CardActions>
-            <Button size="small">Cadastra-se</Button>
-          </CardActions>
         </Card>
       </Grid>
     </Grid>
