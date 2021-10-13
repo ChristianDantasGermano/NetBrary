ed4c1537cfd7182ffa702d6cb8785bfface19424
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-13 20:13:21 -0300

    Adicionado rota da tela autenticação

--- Front-End/netbrary/src/Router.js
+++ Front-End/netbrary/src/Router.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
+import Authentication from "./Screens/Authentication";
 import Home from "./Screens/Home";
 import Logon from "./Screens/Logon/index";
 import Register from "./Screens/Register";
@@ -11,7 +12,8 @@ const Routes = () => {
        <BrowserRouter>
            <Route component = { Home }  path="/" exact />
            <Route component = { Logon }  path="/logon"  />
-           <Route component = { Register }  path= "/register"  />
+           <Route component = { Register }  path= "/register"/>
+           <Route component = { Authentication }  path= "/authentication"/>
        </BrowserRouter>
    )
 }
