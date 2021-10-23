5092a743126bab9eb097cd8b29eaebf1c5cacc5c
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-23 12:54:54 -0300

    Criação da tela Admin

---
+++ Front-End/netbrary/src/Screens/Admin/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavbarAdmin from "../../Components/NavbarAdmin/navbar";
+import Books from "./Pages/Books";
+
+
+export default function Admin() {
+
+  return (
+    <div>
+      <NavbarAdmin />
+      <Books/>
+    </div>
+  );
+}
