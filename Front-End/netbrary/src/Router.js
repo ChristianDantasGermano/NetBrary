130dbea66b4ca882126e97577de12bd38da9856e
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-23 12:57:33 -0300

    Adicionado as rotas das novas telas

--- Front-End/netbrary/src/Router.js
+++ Front-End/netbrary/src/Router.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
+import AdminHome from "./Screens/Admin";
 
 import Authentication from "./Screens/Authentication";
 import Home from "./Screens/Home";
 import Logon from "./Screens/Logon/index";
+import Profile from "./Screens/Profile";
+import RegisterBooks from "./Screens/Admin/Pages/Books/RegisterBooks";
 import Register from "./Screens/Register";
 
 
@@ -14,6 +17,9 @@ const Routes = () => {
            <Route component = { Logon }  path="/logon"  />
            <Route component = { Register }  path= "/register"/>
            <Route component = { Authentication }  path= "/authentication"/>
+           <Route component = { Profile }  path= "/profile"/>
+           <Route component = { AdminHome }  path= "/admin"/>
+           <Route component = { RegisterBooks }  path= "/registerBooks"/>
        </BrowserRouter>
    )
 }
