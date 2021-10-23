cf7a2c146801111ee8aad3aadde7177e0bc26751
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-23 12:58:16 -0300

    Criado base do back-end em Spring

---
+++ Back-End/netBrary/src/main/java/br/fatec/netBrary/NetBraryApplication.java
@@ -0,0 +1,13 @@
+package br.fatec.netBrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetBraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetBraryApplication.class, args);
+	}
+
+}
