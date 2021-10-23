1cae4fd49403c4efcf929c5b9654aa0c3105514a
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-10-23 12:52:17 -0300

    Criação da visualização de livros

--- Front-End/netbrary/src/Screens/Home/index.js
+++ Front-End/netbrary/src/Screens/Home/index.js
@@ -1,12 +1,29 @@
 import React from "react";
-import PrimarySearchAppBar from "../../Components/Navbar/navbar";
+import NavBar from "../../Components/Navbar/navbar";
+import Box from "@material-ui/core/Box";
+
+import itemData from "../../Models/BooksData";
+import SingleContent from "../../Components/SingleContent/SingleContent";
+
+import "./styles.css";
 
 const Home = () => {
   return (
-    <div>
-      <PrimarySearchAppBar />
-    </div>
+    <Box>
+      <NavBar />
+      <div className="trending">
+        {itemData &&
+          itemData.map((c) => (
+            <SingleContent
+              key={c.img}
+              id={c.title}
+              poster={c.img}
+              title={c.title}
+              media_type="movie"
+            />
+          ))}
+      </div>
+    </Box>
   );
 };
-
 export default Home;
