ac295fc4155a1a386394974ab87c899ee49445d8
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-11-05 11:40:01 -0300

    Ajuste no back-end

---
+++ Back-End/netbrary/src/main/java/br/com/fatec/netbrary/NetbraryApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.netbrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbraryApplication.class, args);
+	}
+
+}
---
+++ Back-End/netbrary/src/test/java/br/com/fatec/netbrary/NetbraryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.netbrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
