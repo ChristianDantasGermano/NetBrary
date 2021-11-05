92beaac9f35be29b6f240b3b36e49d5e05450080
Christian Dantas Germano <christian.dantas.germano@gmail.com>
2021-11-05 11:41:12 -0300

    modificações nos modelos de dados exemplos

---
+++ Front-End/netbrary/src/Models/MyBooks.js
@@ -0,0 +1,14 @@
+const MybooksData = [
+  {
+    img: "https://a-static.mlcdn.com.br/618x463/livro-a-menina-que-roubava-livros/magazineluiza/088610800/93fc0dd2c4155d10795fb72f8d7f39c3.jpg",
+    title: "A menina que roubava livros",
+    author: "Markus Zusak",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/91dSMhdIzTL.jpg",
+    title: "A games of Thrones",
+    author: "George R.R.Martin",
+  },
+];
+
+export default MybooksData;
